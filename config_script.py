3edf3fd5c16cfd510ff758877afe9af63afbd3ae
Benjamin Vogt <bvogt7503@gmail.com>
2025-06-11 11:03:39 +0200

    test

--- config_script.py
+++ config_script.py
@@ -4,6 +4,7 @@ import optax
 import math
 import os
 
+# test commit
 
 def init_params(key, n_bg, n_nm, g_bg, g_nm, input_dim, output_dim):
     # for now assume Th/BG/C are same size, g is the same for all weight matrices
